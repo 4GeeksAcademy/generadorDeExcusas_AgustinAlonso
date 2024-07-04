b1434c342ae2bc4eba011f7ebad020a8bf5c762f
Agustin2799 <140901456+Agustin2799@users.noreply.github.com>
2024-07-04 13:55:28 +0000

    Refactorizando con buenas prácticas

--- src/app.js
+++ src/app.js
@@ -1,21 +1,42 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+
+/* ===> BUENAS PRÁCTICAS <=== 
+
+Tanto en el archivo app.js como en index.html fueron modificados para reflejar buenas prácticas de codificación, ya que éste ejercicio no presenta ninguna estructura condicional if con buenas prácticas, dejaré un ejemplo a continuación de lo entendido durante la clase, pero que no afecta la funcionalidad del ejercico.
+
+Buenas prácticas que se aplicaron:
+  - La estructura condicional if se escribió de forma concisa si el uso excesivo de de {}.
+  - Se identó correctamente el código html y js.
+  - Todas las variables, arrays y funciones fueron declaradas con nombres descriptivos y empiezan con 
+    minúsculas, no incluyen tildes y si se componen de más de una palabra, cada palabra después de la primera empieza con mayúscula.
+  - Se comentó el código correctamente.
+  - Se usó el método de concatenación template literals.
+*/
+let llueveAfuera = true;
+let haceFrio = false;
+
+if (llueveAfuera && haceFrio) console.log("Hay que salir abrigado y con paragüas");
+else if (llueveAfuera && !haceFrio) console.log("Salir de remera a mojarse con la lluvia");
+else if (!llueveAfuera && haceFrio) console.log("Hay que salir abrigado")
+else console.log('Vamos a la plaza a pasar la tarde, no  hace frío y no está lloviendo')
 
 //Obtenemos el elemento html excusa
 let excusa = document.getElementById("excusa");
 
+//Los siguientes cuatro arrays contienen los elementos para cada parte de la excusa que seran seleccionados aleatoriamente.
 const quienArray = [
   "Mi vecino",
   "Mi primo",
   "Mi gato",
   "Mi perro",
   "Mi llama",
-  "Mi Novia"
+  "Mi Novia",
 ];
+
 const accionArray = ["rompió", "quemó", "tiró", "robó", "ensució", "escondió"];
 
 const queArray = [
@@ -24,26 +45,21 @@ const queArray = [
   "mi mochila",
   "mi Auto",
   "mi pantalón",
-  "mi almuerzo"
+  "mi almuerzo",
 ];
+
 const cuandoArray = [
   "mientras dormía",
   "mientras estaba en el gimnasio",
   "durante el almuerzo",
   "cuando fui al mercado",
   "mientras me bañaba",
-  "durante el desayuno"
+  "durante el desayuno",
 ];
-//Arrays de arrays
-const Arrays = [quienArray, accionArray, queArray, cuandoArray];
 
-//Función que devuelve un número ramdom y recibe un el número máximo que puede devolver
-function ramdom(maxNumInt) {
-  let num = Math.round(Math.random() * maxNumInt);
-  return num;
-}
-
+//Cada vez que la página cargue, se ejecuta la siguiente función.
 window.onload = function() {
+  //Asignamos aleatoriamente un valor a cada parte de la excusa.
   let quien = quienArray[Math.floor(Math.random() * quienArray.length)];
   let accion = accionArray[Math.floor(Math.random() * accionArray.length)];
   let que = queArray[Math.floor(Math.random() * queArray.length)];
